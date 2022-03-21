30669650ba574279b2d38cce578769bbda009504
Fabio Azevedo <fabioazevedo0619@gmail.com>
2022-03-21 20:22:10 +0000

    Added basic landing page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,7 +1,13 @@
+import { Routes, Route } from "react-router-dom";
+import { Landing } from "./pages/Landing";
 import "./App.css";
 
 function App() {
-  return <div></div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Landing />} />
+    </Routes>
+  );
 }
 
 export default App;
