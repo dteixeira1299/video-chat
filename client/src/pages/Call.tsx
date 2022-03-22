5ccba78178984cf84bbbc0ee8c5058a374a8a88c
Diogo Teixeira <diogoteixeira1299@gmail.com>
2022-03-22 12:38:55 +0000

    select box

--- client/src/pages/Call.tsx
+++ client/src/pages/Call.tsx
@@ -1,6 +1,7 @@
 import { Component, createRef, RefObject } from "react";
 import styles from "../styles/Call.module.css";
 
+
 export class CallPage extends Component {
 	private videoRef: RefObject<HTMLVideoElement>;
 
@@ -34,7 +35,28 @@ export class CallPage extends Component {
 	render(): JSX.Element {
 		return (
 			<div>
+
+				<h1>Select sources &amp; outputs</h1>
+
+				<p>Get available audio, video sources and audio output devices from <code>mediaDevices.enumerateDevices()</code>
+					then set the source for <code>getUserMedia()</code> using a <code>deviceId</code> constraint.</p>
+				<p><b>Note:</b> without permission, the browser will restrict the available devices to at most one per type.</p>
+
+				<div>
+					<label>Audio input source: </label><select id="audioSource"></select>
+				</div>
+
+				<div>
+					<label>Audio output destination: </label><select id="audioOutput"></select>
+				</div>
+
+				<div>
+					<label>Video source: </label><select id="videoSource"></select>
+				</div>
+
+
 				<video ref={this.videoRef} playsInline autoPlay></video>
+
 			</div>
 		)
 	}
