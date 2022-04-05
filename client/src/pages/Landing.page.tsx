1809a3d08c96ffd734cc86dfaaa82cc52f302f3e
Alexandre Soares Oliveira <A033327@ipmaia.pt>
2022-04-05 09:07:56 +0100

    Changed to stateless component

--- client/src/pages/Landing.page.tsx
+++ client/src/pages/Landing.page.tsx
@@ -1,50 +1,54 @@
-import { Component } from "react";
-import React from 'react'
+import React, { ChangeEvent } from "react";
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import styles from "../styles/Landing.module.css";
 
-// TODO: Change to class when it's needed to add setters
-interface LandingPageModel {
-  username?: string;
-  call: {
-    uuid?: string;
-    username?: string;
+export const LandingPage = () => {
+  const [username, setUsername] = useState<string>();
+  const [callUUID, setCallUUID] = useState<string>();
+  const [callUsername, setCallUsername] = useState<string>();
+
+  const navigate = useNavigate();
+
+  const updateUsername = (event: ChangeEvent<HTMLInputElement>) => {
+    setUsername(event.target.value);
   };
-}
 
-export class LandingPage extends Component<{}, LandingPageModel> {
-  constructor(props: {}) {
-    super(props);
-    this.state = { call: {} };
-  }
+  const startCall = () => {
+    if (username) {
+      navigate("/call");
+    }
+  };
 
-  render() {
-    return (
-      <div className={styles["landing-page-container"]}>
-        <div className={styles["option-container"]}>
-          <input
-            type="text"
-            placeholder="Username"
-            className="m-5"
-            value={this.state.username}
-          />
-          <button className="m-5">Start Call</button>
-        </div>
-        <div className={styles["option-container"]}>
-          <input
-            type="text"
-            placeholder="Username"
-            className="m-5"
-            value={this.state.call.username}
-          />
-          <input
-            type="text"
-            placeholder="Call Id"
-            className="m-5"
-            value={this.state.call.uuid}
-          />
-          <button className="m-5">Enter Call</button>
-        </div>
+  return (
+    <div className={styles["landing-page-container"]}>
+      <div className={styles["option-container"]}>
+        <input
+          type="text"
+          placeholder="Username"
+          className="m-5"
+          onChange={updateUsername}
+          value={username}
+        />
+        <button onClick={startCall} className="m-5">
+          Start Call
+        </button>
       </div>
-    );
-  }
-}
+      <div className={styles["option-container"]}>
+        <input
+          type="text"
+          placeholder="Username"
+          className="m-5"
+          value={callUsername}
+        />
+        <input
+          type="text"
+          placeholder="Call Id"
+          className="m-5"
+          value={callUUID}
+        />
+        <button className="m-5">Enter Call</button>
+      </div>
+    </div>
+  );
+};
