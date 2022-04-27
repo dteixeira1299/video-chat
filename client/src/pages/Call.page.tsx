0d097468d19d98c2519508148ac36935333e985a
Fabio Azevedo <fabioazevedo0619@gmail.com>
2022-04-27 19:50:54 +0100

    Imports fix

--- client/src/pages/Call.page.tsx
+++ client/src/pages/Call.page.tsx
@@ -1,31 +1,19 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Container, Row, Col, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
-import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
-import React, { Component, createRef, RefObject, ChangeEvent, useState, useRef, useEffect } from "react";
-import { SelectComponent, SelectOption } from "../components/Select.component";
+import { Button } from "react-bootstrap";
+import React, { useState, useRef, useEffect } from "react";
 import styles from "../styles/Call.module.css";
 import { useNavigate } from "react-router-dom";
 
-interface CallPageModel {
-  stream?: MediaStream;
-}
-
-interface HTMLCallElement extends HTMLVideoElement {
-  setSinkId(id: string): void;
-}
-
 export const CallPage = () => {
-
   const [stream, setStream] = useState<MediaStream>();
   const videoRef = useRef<any>();
   const navigate = useNavigate();
-  
+
   useEffect(() => {
     getUserMedia()
-    .then(startUserMedia)
-    .catch((error: Error) => console.log(error));
-  }, [])
+      .then(startUserMedia)
+      .catch((error: Error) => console.log(error));
+  }, []);
 
   const toogleMicrophone = (): void => {
     if (stream) {
@@ -37,7 +25,7 @@ export const CallPage = () => {
 
   //TO DO - FAZER PEDIDO AO ENDPOINT PARA TERMINAR A CALL E TERMINAR CONEXÃO DE WEBSOCKETS
   const endCall = (): void => {
-    navigate("/")
+    navigate("/");
   };
 
   const toogleVideo = (): void => {
@@ -60,50 +48,41 @@ export const CallPage = () => {
   };
 
   const getUserMedia = async (): Promise<MediaStream> => {
-    const hasVideo = (await getUserDevices()).filter(
-      device => device.kind == "videoinput"
-    );
+    const hasVideo =
+      (await getUserDevices()).filter(device => device.kind == "videoinput")
+        .length > 0;
 
     return navigator.mediaDevices.getUserMedia({
-      video: hasVideo.length > 0,
+      video: hasVideo,
       audio: true
     });
   };
 
-
   return (
     <div className={styles["main-container"]}>
-    <div className={styles["video-container"]}>
-      <video
-        ref={videoRef}
-        className={styles["video"]}
-        playsInline
-        autoPlay
-      ></video>
-    </div>
-    <div>
-    <Button 
-        onClick={toogleMicrophone}
-        variant="outline-primary"
-        className="mt-4"
-      >
-        Disable Mic
-      </Button>
-      <Button
-        onClick={toogleVideo}
-        variant="dark"
-        className="mt-4"
-      >
-        Disable Cam
-      </Button>
-      <Button
-        onClick={endCall}
-        variant="dark"
-        className="mt-4"
-      >
-        End Call
-      </Button>
-    </div>
+      <div className={styles["video-container"]}>
+        <video
+          ref={videoRef}
+          className={styles["video"]}
+          playsInline
+          autoPlay
+        ></video>
+      </div>
+      <div>
+        <Button
+          onClick={toogleMicrophone}
+          variant="outline-primary"
+          className="mt-4"
+        >
+          Disable Mic
+        </Button>
+        <Button onClick={toogleVideo} variant="dark" className="mt-4">
+          Disable Cam
+        </Button>
+        <Button onClick={endCall} variant="dark" className="mt-4">
+          End Call
+        </Button>
+      </div>
     </div>
   );
-}
+};
