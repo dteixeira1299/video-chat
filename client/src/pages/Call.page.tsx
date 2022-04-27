616873d4d7bad76962775d383c91a5543016d671
Hypn0iid <igorortizsousar@hotmail.com>
2022-04-27 19:45:15 +0100

    botao disable mic/cam.

--- client/src/pages/Call.page.tsx
+++ client/src/pages/Call.page.tsx
@@ -1,10 +1,11 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row, Col, Button } from "react-bootstrap";
 import "bootstrap/dist/css/bootstrap.min.css";
 import { solid } from "@fortawesome/fontawesome-svg-core/import.macro";
-import React, { Component, createRef, RefObject, ChangeEvent } from "react";
+import React, { Component, createRef, RefObject, ChangeEvent, useState, useRef, useEffect } from "react";
 import { SelectComponent, SelectOption } from "../components/Select.component";
 import styles from "../styles/Call.module.css";
+import { useNavigate } from "react-router-dom";
 
 interface CallPageModel {
   stream?: MediaStream;
@@ -14,52 +15,95 @@ interface HTMLCallElement extends HTMLVideoElement {
   setSinkId(id: string): void;
 }
 
-export class CallPage extends Component<{}, CallPageModel> {
-  private videoRef: RefObject<HTMLCallElement>;
+export const CallPage = () => {
 
-  constructor(props: {}) {
-    super(props);
-    this.videoRef = createRef();
-  }
+  const [stream, setStream] = useState<MediaStream>();
+  const videoRef = useRef<any>();
+  const navigate = useNavigate();
+  
+  useEffect(() => {
+    getUserMedia()
+    .then(startUserMedia)
+    .catch((error: Error) => console.log(error));
+  }, [])
 
-  componentDidMount = (): void => {
-    this.getUserMedia()
-      .then(this.startUserMedia)
-      .catch((error: Error) => console.log(error));
-  };
-
-  private startUserMedia = (mediaStream: MediaStream): void => {
-    if (this.videoRef.current) {
-      this.videoRef.current.srcObject = mediaStream;
-      this.setState({ stream: mediaStream });
+  const toogleMicrophone = (): void => {
+    if (stream) {
+      stream
+        .getAudioTracks()
+        .forEach(track => (track.enabled = !track.enabled));
     }
   };
 
-  private getUserDevices = (): Promise<MediaDeviceInfo[]> => {
+  //TO DO - FAZER PEDIDO AO ENDPOINT PARA TERMINAR A CALL E TERMINAR CONEXÃO DE WEBSOCKETS
+  const endCall = (): void => {
+    navigate("/")
+  };
+
+  const toogleVideo = (): void => {
+    if (stream) {
+      stream
+        .getVideoTracks()
+        .forEach(track => (track.enabled = !track.enabled));
+    }
+  };
+
+  const startUserMedia = (mediaStream: MediaStream): void => {
+    if (videoRef.current) {
+      videoRef.current.srcObject = mediaStream;
+      setStream(mediaStream);
+    }
+  };
+
+  const getUserDevices = (): Promise<MediaDeviceInfo[]> => {
     return navigator.mediaDevices.enumerateDevices();
   };
 
-  private getUserMedia = async (): Promise<MediaStream> => {
-    const hasVideo = (await this.getUserDevices()).filter(
+  const getUserMedia = async (): Promise<MediaStream> => {
+    const hasVideo = (await getUserDevices()).filter(
       device => device.kind == "videoinput"
     );
 
     return navigator.mediaDevices.getUserMedia({
-      video: true,
+      video: hasVideo.length > 0,
       audio: true
     });
   };
 
-  render = (): JSX.Element => {
-    return (
-      <div className={styles["video-container"]}>
-        <video
-          ref={this.videoRef}
-          className={styles["video"]}
-          playsInline
-          autoPlay
-        ></video>
-      </div>
-    );
-  };
+
+  return (
+    <div className={styles["main-container"]}>
+    <div className={styles["video-container"]}>
+      <video
+        ref={videoRef}
+        className={styles["video"]}
+        playsInline
+        autoPlay
+      ></video>
+    </div>
+    <div>
+    <Button 
+        onClick={toogleMicrophone}
+        variant="outline-primary"
+        className="mt-4"
+      >
+        Disable Mic
+      </Button>
+      <Button
+        onClick={toogleVideo}
+        variant="dark"
+        className="mt-4"
+      >
+        Disable Cam
+      </Button>
+      <Button
+        onClick={endCall}
+        variant="dark"
+        className="mt-4"
+      >
+        End Call
+      </Button>
+    </div>
+    </div>
+  );
 }
