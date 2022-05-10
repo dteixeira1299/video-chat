a0f4daa4037a527ae2ab6aa887aaac7f8bf9aade
Diogo Teixeira <diogoteixeira1299@gmail.com>
2022-05-10 10:18:11 +0100

    botão para entrar na call na página waiting

--- server/src/database-connection.ts
+++ server/src/database-connection.ts
@@ -6,7 +6,7 @@ export const DatabaseConnection = new DataSource({
   host: "localhost",
   port: 3306,
   username: "root",
-  password: "root",
+  password: "",
   database: "chat",
   synchronize: true,
   entities: [Call],
